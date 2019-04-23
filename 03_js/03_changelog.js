1ad97a52a2d1556da0d63e61dbc09dee16c3410a
Jesse CXElegance <contact@cxelegance.com>
2019-04-23 18:36:54 -0300

    better comment block for changelog

--- 03_js/03_changelog.js
+++ 03_js/03_changelog.js
@@ -1,19 +1,18 @@
-// *****
-// Change Log (changelog) for CXElegance.com
-//
-// VERSION: 1.5.3; 2013-04-27
-//	- started the changelog (this JS doc)
-//	- stopped adapting to narrow screens, for now, because features need improving to make this "nice"
-//		- for smartphones and tablets
-//	- moved TODO list into main JS/jQuery source (was in HTML)
-//	- moved all legacy and embarrassing old code into a namespaced object
-//	- added 'use strict' for my coding purposes
-//	- removed "popup" (modal, fixed, absolute)
-//		- replaced with height-growing, CSS-able content that animates in-page
-//	- removed animated window timer junk that initiates at load (flashing links)
-//
-// *****
+/*
+ * Change Log (changelog for CXElegance.com)
+ *
+ * VERSION: 1.5.3; 2013-04-27
+ *    - started the changelog (this JS doc)
+ *    - stopped adapting to narrow screens, for now, because features need improving to make this "nice"
+ *       - for smartphones and tablets
+ *    - moved TODO list into main JS/jQuery source (was in HTML)
+ *    - moved all legacy and embarrassing old code into a namespaced object
+ *    - added 'use strict' for my coding purposes
+ *    - removed "popup" (modal, fixed, absolute)
+ *       - replaced with height-growing, CSS-able content that animates in-page
+ *    - removed animated window timer junk that initiates at load (flashing links)
+ */
 
 (function (whatisthis) {
-	// alert ('this is just a ' + whatisthis); // just goofing around here
+	if(whatisthis === 'changelog') return true;
 }) ('changelog');
