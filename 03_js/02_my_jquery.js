1055c6cece7b8d3256dab9ec960124243e5a9039
Jesse CXElegance <contact@cxelegance.com>
2019-06-12 12:07:41 -0300

    index: explain sole entity not organization

--- 03_js/02_my_jquery.js
+++ 03_js/02_my_jquery.js
@@ -72,12 +72,14 @@
  * Milestones for Version 2.0
  *
  *    - Version 2.2.2:
- *       - revisit the original colour schemes and be sure they're implemented correctly in the current
- *         three pages
+ *    	 - add copy to explain that CXElegance is a sole trading entity, not an organization
  *       - add service: copy editing for website
  *       - bugfix: links not working
+ *       - bugfix: fix top margins of detal blocks
  *
  *    - Version 2.2.3:
+ *       - revisit the original colour schemes and be sure they're implemented correctly in the current
+ *         three pages
  *       - general refactoring
  *       	- rid of jQuery plugin that was made for page scrolling
  *       	- rid of "CXEPage", but then recreate the namespace for any public-facing methods or props required
