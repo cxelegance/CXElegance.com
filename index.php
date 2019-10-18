ab51083ce84b8ab2e471a2e3d504b36a7e2314c8
Jesse CXElegance <contact@cxelegance.com>
2019-10-18 14:00:10 -0300

    added some TODO for next time

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+//TODO: get this working; find out where this ENV var is and sense http/s
+//TODO: find all hardcoded http/s and put in the sensed version.
+//TODO: recreate the app at Heroku and git branch --set-upstream-to=https://git.heroku.com/<insert the new name here>
+//TODO: be sure that versioning is working when building; I don't want it autoincremented.
 $isHTTPS = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' && !empty($_SERVER['HTTPS']);
 ?>
 
